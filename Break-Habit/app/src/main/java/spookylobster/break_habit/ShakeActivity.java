5743b55c431b1d433e059e65efbee7845526dbb1
Ewong3 <eric3@ualberta.ca>
2017-01-28 18:31:21 -0700

    Shake detection

--- Break-Habit/app/src/main/java/spookylobster/break_habit/ShakeActivity.java
+++ Break-Habit/app/src/main/java/spookylobster/break_habit/ShakeActivity.java
@@ -1,6 +1,7 @@
 package spookylobster.break_habit;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
@@ -8,16 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.hardware.Sensor;
-import android.hardware.SensorEventListener;
-import android.view.View;
 import android.widget.TextView;
 
 public class ShakeActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager SensorManager;
     private Sensor Accelerometer;
     private float previousZ = 0;
-    private Integer shakeCounter = 10;
+    private Integer shakeCounter = 50;
     private TextView counter;
+    private long lastUpdate = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +28,26 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
         SensorManager.registerListener(this, Accelerometer , SensorManager.SENSOR_DELAY_NORMAL);
 
         counter = (TextView) findViewById(R.id.shake_counter);
-        counter.setText(shakeCounter.toString());
-
-
+        updateCounterText();
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mySensor = sensorEvent.sensor;
 
+        long curTime = System.currentTimeMillis();
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float z = sensorEvent.values[2];
+            float z = sensorEvent.values[1];
+            System.out.print("Shake is " + z);
+            if ((curTime - lastUpdate) > 100) {
 
-            if (z != previousZ) {
+                float speed = Math.abs(z - previousZ)/ (curTime - lastUpdate) * 10000;
+                lastUpdate = curTime;
+
+                if ( 500 < speed) {
+                    shake();
+                }
                 previousZ = z;
-
             }
         }
     }
@@ -52,5 +57,20 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
 
     }
 
+    public void shake() {
+        if (shakeCounter == 0){
+            stopShakeActivity();
+        }
+        shakeCounter -= 1;
+        updateCounterText();
+    }
 
+    public void updateCounterText() {
+        counter.setText(shakeCounter.toString());
+    }
+
+    public void stopShakeActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
