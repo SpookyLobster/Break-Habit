ff72178f8103e600bcd0ee2222c9d07b7a5687d3
Ewong3 <eric3@ualberta.ca>
2017-01-29 01:28:36 -0700

    sql

--- Break-Habit/app/src/main/java/spookylobster/break_habit/SQLhandler.java
+++ Break-Habit/app/src/main/java/spookylobster/break_habit/SQLhandler.java
@@ -30,8 +30,8 @@ public class SQLhandler extends SQLiteOpenHelper{
         db.execSQL("create table setting "+
                 "(method varchar(100) primary key, number integer)");
         //set default value for settings
-        db.execSQL("insert into setting (method, number) values('SetShake',25)");
-        db.execSQL("insert into setting (method, number) values('SetPushup',10)");
+        db.execSQL("insert into setting (method, number) values(\"SetShake\",25)");
+        db.execSQL("insert into setting (method, number) values(\"SetPush\",10)");
         //db.execSQL("insert into setting values('SetTime',00:05)");
 
     }
@@ -53,16 +53,31 @@ public class SQLhandler extends SQLiteOpenHelper{
     }
     public void updateSetting(int number, String method){
         //db.update("setting", contentValues, "_id="+id,null);
-        this.getWritableDatabase().execSQL("UPDATE SETTING SET number="+number+" where method=\""+method+"\"");
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("UPDATE setting SET number="+number+" where method=\""+method+"\"");
+        db.close();
     }
 
     //Getting the userinput number
     public int getData(String method) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("select * from setting where method = \"" + method + "\"", null);
+        System.out.print(method);
+        Cursor res;
+        //if (method.equals("SetPush")) {
+            res = db.rawQuery("select * from setting where method = \"SetPush\"", null);
+        //} else if (method.equals("SetShake")){
+   //         res = db.rawQuery("select * from setting where method = \"SetShake\"", null);
+        //} else {
+ //           res = null;
+       // }
+        //cursor res = db.rawQuery("select * from setting where method = \"" + method + "\"", null);
 
-        //Cursor res = db.rawQuery("select * from setting where method = \"SetShake\"", null);
-        res.moveToFirst();
-        return res.getInt(1);
+        //Cursor res = db.rawQuery("select * from setting where method = \"SetPush\"", null);
+        //Cursor res = db.rawQuery("select * from setting", null);
+        if(res != null) {
+            res.moveToFirst();
+            return res.getInt(1);
+        }
+        return -1;
     }
 }
