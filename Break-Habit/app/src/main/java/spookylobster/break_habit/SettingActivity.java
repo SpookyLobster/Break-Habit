1541a1829e1f216ccd02c6da267af4b399d32ba4
peijen <peijen@ualberta.ca>
2017-01-28 18:51:37 -0700

    some more setting stuff

--- Break-Habit/app/src/main/java/spookylobster/break_habit/SettingActivity.java
+++ Break-Habit/app/src/main/java/spookylobster/break_habit/SettingActivity.java
@@ -1,7 +1,11 @@
 package spookylobster.break_habit;
 
+import android.content.Intent;
+import android.location.SettingInjectorService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+
+
+
+        final Button back= (Button) findViewById(R.id.backbutton);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+
     }
+
+
+
 }
